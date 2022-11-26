51bec6cff5c2ca5e2bb6329d6f43f4ad6b68d943
iswilljr <iswilljr@gmail.com>
2022-11-25 20:33:05 -0500

    feat: creating character with episode and location relation

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -4,9 +4,11 @@ import { CharacterController } from "./character.controller";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Character } from "./entities/character.entity";
+import { EpisodeModule } from "episode/episode.module";
+import { LocationModule } from "location/location.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Character])],
+  imports: [TypeOrmModule.forFeature([Character]), EpisodeModule, LocationModule],
   controllers: [CharacterController],
   providers: [CharacterService, ConfigService],
   exports: [TypeOrmModule, CharacterService],
--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -2,23 +2,57 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CRUDService } from "common/classes/crud.service";
 import { CharacterResponse } from "common/interfaces/character.interface";
-import { Repository } from "typeorm";
+import { Episode } from "episode/entities/episode.entity";
+import { Location } from "location/entities/location.entity";
+import { DeepPartial, In, Repository } from "typeorm";
 import { CreateCharacterDto } from "./dto/create-character.dto";
 import { Character } from "./entities/character.entity";
 import { transformCharacter } from "./helpers/transform-character.helper";
 
 @Injectable()
 export class CharacterService extends CRUDService<Character, CharacterResponse> {
-  constructor(@InjectRepository(Character) characterRepository: Repository<Character>) {
+  constructor(
+    @InjectRepository(Character) characterRepository: Repository<Character>,
+    @InjectRepository(Episode) private readonly episodeRepository: Repository<Episode>,
+    @InjectRepository(Location) private readonly locationRepository: Repository<Location>
+  ) {
     super(characterRepository, { loggerName: "CharacterService", transformObj: transformCharacter });
   }
 
-  createCharacter({
+  async createCharacter(create: CreateCharacterDto): Promise<CharacterResponse> {
+    const character = await this.getCharacter(create);
+
+    // eslint-disable-next-line @typescript-eslint/return-await
+    return this.create(character);
+  }
+
+  async createCharacters(create: CreateCharacterDto[]): Promise<CharacterResponse[]> {
+    const characters = await Promise.all(create.map((character) => this.getCharacter(character)));
+
+    // eslint-disable-next-line @typescript-eslint/return-await
+    return this.create(characters) as unknown as Promise<CharacterResponse[]>;
+  }
+
+  private async getCharacter({
     episode,
-    locationId,
     originId,
+    locationId,
     ...createCharacterDto
-  }: CreateCharacterDto): Promise<CharacterResponse> {
-    return this.create(createCharacterDto);
+  }: CreateCharacterDto): Promise<DeepPartial<Character>> {
+    const [episodeObj, originAndlocation] = await Promise.all([
+      this.episodeRepository.findBy({ id: In(episode) }),
+      originId === locationId
+        ? this.locationRepository.findOneBy({ id: originId })
+        : this.locationRepository.findBy({ id: In([originId, locationId]) }),
+    ]);
+
+    const find = (id: number): Location => (originAndlocation as Location[]).find((value) => value.id === id);
+
+    return {
+      episode: episodeObj,
+      location: Array.isArray(originAndlocation) ? find(locationId) : originAndlocation,
+      origin: Array.isArray(originAndlocation) ? find(originId) : originAndlocation,
+      ...createCharacterDto,
+    };
   }
 }
