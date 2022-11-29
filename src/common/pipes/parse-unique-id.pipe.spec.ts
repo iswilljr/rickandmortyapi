17e0632c0d371cbc70ac7c4b7d1825cd3c0a1925
iswilljr <iswilljr@gmail.com>
2022-11-29 11:58:51 -0500

    chore: add test for character, episode, location and parse-unique-id-pipe

---
+++ src/common/pipes/parse-unique-id.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParseUniqueIdPipe } from "./parse-unique-id.pipe";
+
+describe("CcPipe", () => {
+  it("should be defined", () => {
+    expect(new ParseUniqueIdPipe()).toBeDefined();
+  });
+});
