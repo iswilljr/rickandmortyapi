8b72721633fc2219a02ec069d2710498b95d3037
iswilljr <iswilljr@gmail.com>
2023-02-17 09:29:26 -0500

    feat: using getUrl helper to send all endpoints

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { getUrl } from "./common/helpers/get-url.helper";
 import type { Endpoints } from "common/interfaces";
 
 @Injectable()
 export class AppService {
-  baseURL: string;
-
-  constructor(configService: ConfigService) {
-    this.baseURL = new URL(configService.get("BASE_URL")!).toString();
-  }
-
   root(): Partial<Record<Endpoints, string>> {
     return {
-      character: `${this.baseURL}/character`,
-      episode: `${this.baseURL}/episode`,
-      location: `${this.baseURL}/location`,
+      character: getUrl({ endpoint: "character" }),
+      location: getUrl({ endpoint: "location" }),
+      episode: getUrl({ endpoint: "episode" }),
     };
   }
 }
